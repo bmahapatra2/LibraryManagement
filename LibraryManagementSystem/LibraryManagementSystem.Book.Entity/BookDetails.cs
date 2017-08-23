866bfee60b483d45d78df75289edb1235361f47b
Bandana Mahapatra <bmahapatra@tavisca.com>
2017-08-23 14:46:32 +0530

    Files Added

---
+++ LibraryManagementSystem/LibraryManagementSystem.Book.Entity/BookDetails.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Book.Entity
+{
+    public class BookDetails
+    {
+        private string _bookname;
+        private string _author;
+        private Genre _genre;
+        private int _bookid;
+
+        public string BookName { get; set; }
+
+        public string Author { get; set; }
+        
+        public Genre Genre { get; set; }
+
+        public int BookId { get; set; }
+
+        public BookDetails(string BookName, string Author, Genre Genre, int BookId)
+        {
+            _bookname = BookName;
+            _author = Author;
+            _genre = Genre;
+            _bookid = BookId;
+        }
+
+        public BookDetails()
+        {
+        }
+
+        public override string ToString()
+        {
+            
+            return _bookname+_author+_genre+_bookid;
+        }
+
+
+    }
+}
---
+++ LibraryManagementSystem/LibraryManagementSystem.IssuedBookData/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryManagementSystem.Book.Entity;
+using LibraryManagementSystem.BookIssueProcess;
+
+
+namespace LibraryManagementSystem.IssuedBookData
+{
+    public class LibraryIssuedBooks
+    {
+        public string[] BookIssued(IssueBook book)
+        {
+            IssueBook books = new IssueBook();
+            books.AddBookDetails();
+            string[] bookdata= books.GetBookDetails().Split(',');
+            return bookdata;
+        }
+    }
+}
